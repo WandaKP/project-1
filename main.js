19a8075838eac1d704e34cedcdfc67afafe5dcd9
Wanda <wandaplaza1031@gmail.com>
2022-12-11 21:08:55 -0500

    Else if

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
-const userChoice = alert(promt("How many rounds do you want to play?"));
-let randomNum = math.floor(math.random()*3);
-const playerChoice = alert(promt("Do you pick rock, paper, or scissors (enter r, p, or s)"));
+let ties = 0
+let wins = 0
+let losses = 0
+const userChoice = prompt("How many rounds do you want to play?");
+let randomNum = Math.floor(Math.random()*3);
+const playerChoice = prompt("Do you pick rock, paper, or scissors (enter r, p, or s)");
 
 let computerChoice;
 if (randomNum == 0) {
     computerChoice = "r";
 } else if (randomNum == 1) {
         computerChoice = "p";
-    }
-else {
+    } else {
     computerChoice = "s"
 }
 
 if (playerChoice === computerChoice) {
     alert("It's a draw!")
-} else if ((playerChoice, computerChoice)) {
+    ties++
+} else if (playerChoice === 'r' && computerChoice === 's') {
     alert("You won!")
+    wins++
+} else if (playerChoice === 'p' && computerChoice === 'r') {
+    alert("You won!")
+    wins++
+} else if (playerChoice === 's' && computerChoice === 'p'){
+    alert("You won!")
+    wins++
 } else {
     alert("Sorry, Computer Wins!")
+    losses++
 }
+
